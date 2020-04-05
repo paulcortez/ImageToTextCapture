8f80fb9eda5703e96fc963aeb3ab204be6a55a8a
Paul Cortez <paulcortez0591@yahoo.com>
2020-04-06 01:06:49 +0800

    final

--- ImageToTextCapture/Home.Designer.cs
+++ ImageToTextCapture/Home.Designer.cs
@@ -28,46 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pnlImageContainer = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imgContainer = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.tbImageZoom = new System.Windows.Forms.TrackBar();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.ofdBrowseFile = new System.Windows.Forms.OpenFileDialog();
+            this.chkReadingTextOnOff = new System.Windows.Forms.CheckBox();
+            this.slicedImage = new System.Windows.Forms.PictureBox();
+            this.txtExtractedFromImage = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.pnlImageContainer.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbImageZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slicedImage)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.txtExtractedFromImage);
+            this.panel1.Controls.Add(this.slicedImage);
+            this.panel1.Controls.Add(this.chkReadingTextOnOff);
+            this.panel1.Controls.Add(this.btnBrowseFile);
+            this.panel1.Controls.Add(this.txtFilePath);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(644, 0);
+            this.panel1.Location = new System.Drawing.Point(708, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(364, 729);
+            this.panel1.Size = new System.Drawing.Size(300, 729);
             this.panel1.TabIndex = 0;
             // 
             // pnlImageContainer
             // 
+            this.pnlImageContainer.AutoScroll = true;
             this.pnlImageContainer.BackColor = System.Drawing.Color.Black;
-            this.pnlImageContainer.Controls.Add(this.pictureBox1);
-            this.pnlImageContainer.Location = new System.Drawing.Point(50, 132);
+            this.pnlImageContainer.Controls.Add(this.imgContainer);
+            this.pnlImageContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlImageContainer.Location = new System.Drawing.Point(0, 0);
             this.pnlImageContainer.Name = "pnlImageContainer";
-            this.pnlImageContainer.Size = new System.Drawing.Size(386, 274);
+            this.pnlImageContainer.Size = new System.Drawing.Size(708, 684);
             this.pnlImageContainer.TabIndex = 1;
             // 
-            // pictureBox1
+            // imgContainer
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgContainer.Location = new System.Drawing.Point(0, 0);
+            this.imgContainer.Name = "imgContainer";
+            this.imgContainer.Size = new System.Drawing.Size(100, 50);
+            this.imgContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgContainer.TabIndex = 0;
+            this.imgContainer.TabStop = false;
+            this.imgContainer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imgContainer_MouseDown);
+            this.imgContainer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imgContainer_MouseMove);
+            this.imgContainer.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imgContainer_MouseUp);
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.tbImageZoom);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 684);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(644, 45);
+            this.panel2.Size = new System.Drawing.Size(708, 45);
             this.panel2.TabIndex = 2;
+            // 
+            // tbImageZoom
+            // 
+            this.tbImageZoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbImageZoom.Location = new System.Drawing.Point(0, 0);
+            this.tbImageZoom.Maximum = 20;
+            this.tbImageZoom.Name = "tbImageZoom";
+            this.tbImageZoom.Size = new System.Drawing.Size(708, 45);
+            this.tbImageZoom.TabIndex = 4;
+            this.tbImageZoom.Value = 10;
+            this.tbImageZoom.Scroll += new System.EventHandler(this.tbImageZoom_Scroll);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(6, 14);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(201, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Location = new System.Drawing.Point(213, 12);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFile.TabIndex = 1;
+            this.btnBrowseFile.Text = "Browse";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // ofdBrowseFile
+            // 
+            this.ofdBrowseFile.FileName = "openFileDialog1";
+            this.ofdBrowseFile.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdBrowseFile_FileOk);
+            // 
+            // chkReadingTextOnOff
+            // 
+            this.chkReadingTextOnOff.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkReadingTextOnOff.Location = new System.Drawing.Point(6, 40);
+            this.chkReadingTextOnOff.Name = "chkReadingTextOnOff";
+            this.chkReadingTextOnOff.Size = new System.Drawing.Size(105, 24);
+            this.chkReadingTextOnOff.TabIndex = 2;
+            this.chkReadingTextOnOff.Text = "Reading Text OFF";
+            this.chkReadingTextOnOff.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkReadingTextOnOff.UseVisualStyleBackColor = true;
+            this.chkReadingTextOnOff.CheckedChanged += new System.EventHandler(this.chkReadingTextOnOff_CheckedChanged);
+            // 
+            // slicedImage
+            // 
+            this.slicedImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.slicedImage.Location = new System.Drawing.Point(6, 70);
+            this.slicedImage.Name = "slicedImage";
+            this.slicedImage.Size = new System.Drawing.Size(282, 282);
+            this.slicedImage.TabIndex = 3;
+            this.slicedImage.TabStop = false;
+            // 
+            // txtExtractedFromImage
+            // 
+            this.txtExtractedFromImage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtExtractedFromImage.Location = new System.Drawing.Point(6, 358);
+            this.txtExtractedFromImage.Multiline = true;
+            this.txtExtractedFromImage.Name = "txtExtractedFromImage";
+            this.txtExtractedFromImage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtExtractedFromImage.Size = new System.Drawing.Size(282, 326);
+            this.txtExtractedFromImage.TabIndex = 4;
             // 
             // Home
             // 
@@ -75,14 +165,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1008, 729);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.pnlImageContainer);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Home";
             this.Text = "Welcome to Image To Text Capture";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Home_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.pnlImageContainer.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbImageZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slicedImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,8 +189,15 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel pnlImageContainer;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgContainer;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar tbImageZoom;
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog ofdBrowseFile;
+        private System.Windows.Forms.CheckBox chkReadingTextOnOff;
+        private System.Windows.Forms.PictureBox slicedImage;
+        private System.Windows.Forms.TextBox txtExtractedFromImage;
     }
 }
 
--- ImageToTextCapture/Home.cs
+++ ImageToTextCapture/Home.cs
@@ -7,19 +7,132 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tesseract;
 
 namespace ImageToTextCapture
 {
     public partial class Home : Form
     {
+        private Point clickPosition;
+        private Point scrollPosition;
+        private Size OriginalSize = new Size();
+        private int cropX, cropY, cropW, cropH;
+        private Pen cropPen;
+
         public Home()
         {
             InitializeComponent();
         }
 
+        private string GetTextFromImage(Bitmap capturedImg)
+        {
+            Enabled = false;
+
+            string text = "";
+
+            var img = new Bitmap(capturedImg);
+            var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+            var page = ocr.Process(img);
+
+            text = page.GetText();
+
+            Enabled = true;
+
+            return text;
+        }
+
         private void Home_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void tbImageZoom_Scroll(object sender, EventArgs e)
+        {
+            double scale = (double)tbImageZoom.Value / 10D;
+
+            this.SuspendLayout();
+            imgContainer.Size = new Size((int)(OriginalSize.Width * scale), (int)(OriginalSize.Height * scale));
+            this.ResumeLayout();
+        }
+
+        private void imgContainer_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (chkReadingTextOnOff.Checked)
+            {
+                cropX = e.X;
+                cropY = e.Y;
+
+                cropPen = new Pen(Color.Red, 1);
+                cropPen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            }
+            else
+                this.clickPosition = e.Location;
+        }
+
+        private void imgContainer_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.SuspendLayout();
+
+            if (e.Button == MouseButtons.Left && chkReadingTextOnOff.Checked)
+            {
+                imgContainer.Refresh();
+                cropW = e.X - cropX;
+                cropH = e.Y - cropY;
+
+                imgContainer.CreateGraphics().DrawRectangle(cropPen, cropX, cropY, cropW, cropH);
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                this.scrollPosition += (Size)clickPosition - (Size)e.Location;
+                this.pnlImageContainer.AutoScrollPosition = scrollPosition;
+            }
+
+            this.ResumeLayout(false);
+        }
+
+        private void imgContainer_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && chkReadingTextOnOff.Checked)
+            {
+                Rectangle rec = new Rectangle(cropX, cropY, cropW, cropH);
+                Bitmap originalImage = new Bitmap(imgContainer.Image, imgContainer.Width, imgContainer.Height);
+                Bitmap newImage = new Bitmap(cropW, cropH);
+                Graphics g = Graphics.FromImage(newImage);
+
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.DrawImage(originalImage, 0, 0, rec, GraphicsUnit.Pixel);
+
+                slicedImage.Image = newImage;
+
+                txtExtractedFromImage.Text = GetTextFromImage(newImage);
+            }
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            ofdBrowseFile.ShowDialog();
+        }
+
+        private void ofdBrowseFile_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFilePath.Text = ofdBrowseFile.FileName;
+
+            Image img = Bitmap.FromFile(txtFilePath.Text);
+
+            imgContainer.Image = img;
+            imgContainer.Size = new Size(img.Width, img.Height);
+            OriginalSize = imgContainer.Size;
+
+            tbImageZoom.Value = 10;
+        }
+
+        private void chkReadingTextOnOff_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkReadingTextOnOff.Checked)
+                chkReadingTextOnOff.Text = "Reading Text ON";
+            else
+                chkReadingTextOnOff.Text = "Reading Text OFF";
         }
     }
 }
