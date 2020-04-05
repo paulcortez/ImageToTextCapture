bc8a44bab6e1bfca203e2e9ea6343fadcb8d4afd
Paul Cortez <paulcortez0591@yahoo.com>
2020-04-05 15:40:52 +0800

    initial design

---
+++ ImageToTextCapture/Home.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ImageToTextCapture
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlImageContainer = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlImageContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(644, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 729);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlImageContainer
+            // 
+            this.pnlImageContainer.BackColor = System.Drawing.Color.Black;
+            this.pnlImageContainer.Controls.Add(this.pictureBox1);
+            this.pnlImageContainer.Location = new System.Drawing.Point(50, 132);
+            this.pnlImageContainer.Name = "pnlImageContainer";
+            this.pnlImageContainer.Size = new System.Drawing.Size(386, 274);
+            this.pnlImageContainer.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 684);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(644, 45);
+            this.panel2.TabIndex = 2;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlImageContainer);
+            this.Controls.Add(this.panel1);
+            this.Name = "Home";
+            this.Text = "Welcome to Image To Text Capture";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.pnlImageContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlImageContainer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
